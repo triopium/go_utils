ecba1e10a93da9161a155c29b70a6d9656d69fa2
Jan Kacaba <jan.kacaba@gmail.com>
2024-05-13 13:57:23 +0200

    Add errlist

--- test/pkg/helper/errlist.go
+++ test/pkg/helper/errlist.go
@@ -8,13 +8,13 @@ import (
 	"github.com/triopium/go_utils/pkg/logging"
 )
 
-var err1 = fmt.Errorf("runner error")
+var err1 = fmt.Errorf("test error")
 
 func Runner() (res error) {
 	errMain := new(helper.ErrMain)
 	defer errMain.Handle(&res)
 	errMain.ErrorRaise(nil)
-	errMain.ErrorAdd(err1)
+	// errMain.ErrorAdd(err1)
 	errMain.ErrorRaise(err1)
 	errMain.ErrorRaise(nil)
 	return errMain.ErrorsReturn()
@@ -24,6 +24,6 @@ func main() {
 	logging.SetLogLevel("-4", "json")
 	err := Runner()
 	if err != nil {
-		slog.Info("fuck", "error", err.Error())
+		slog.Info("main error", "error", err.Error())
 	}
 }
