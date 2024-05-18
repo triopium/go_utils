58234b8552df57b815ed75046c740d3e8b636bf5
Jan Kacaba <jan.kacaba@gmail.com>
2024-05-19 00:09:54 +0200

    WIP: using generic flag option

---
+++ test/pkg/configure2/cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/triopium/go_utils/pkg/configure"
+)
+
+// Build tags set with -ldflags. Cannot set struct fields directly.
+var (
+	BuildGitTag    string
+	BuildGitCommit string
+	BuildBuildTime string
+)
+
+// VersionInfo Binary version info
+var VersionInfo = configure.VersionInfo{
+	Version:   "0.0.1",
+	GitTag:    BuildGitTag,
+	GitCommit: BuildGitCommit,
+}
+
+var commandRootConfig = configure.CommandRoot
+
+func CommandRootRun() {
+	commandRootConfig.VersionInfoAdd(&VersionInfo)
+	commandRootConfig.Init()
+	commandRootConfig.AddSub("dummy", RunCommandDummy)
+	commandRootConfig.RunRoot()
+}
